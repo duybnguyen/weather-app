ed9ae15552f180d50de314f277fa21ec3a72c322
Duy Nguyen <duybnguyen04@gmail.com>
2024-06-10 14:23:05 -0400

    made successful api call

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import './index.css'
 import imageImports from './components/imagesImports'
+import currentApiCall from './components/apiCall'
 
 imageImports()
+
+
+const submitBtn = document.querySelector('.submit')
+
+submitBtn.addEventListener('click', (e) => {
+    const location = document.getElementById('search').value
+    e.preventDefault()
+    currentApiCall(location)
+})
+
+
+
+
+
